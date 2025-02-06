20d0e2f70a900aaa7ee02c0654245ce5dc733417
Quick ⚡ <quick@disroot.org>
2025-02-06 11:56:11 +0530

    vendor: Rebrand lineage props to avoid detection
    Change-Id: Id5eb4545f54c28a476cf32e3d9088d8d9eb816ac

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -57,7 +57,7 @@ PRODUCT_PACKAGES += \
 
 # Legal
 PRODUCT_SYSTEM_PROPERTIES += \
-    ro.lineagelegal.url=https://lineageos.org/legal
+    ro.pixellegal.url=https://lineageos.org/legal
 
 # Media
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
