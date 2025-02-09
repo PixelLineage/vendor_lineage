b88041c4c195dcbb0d3a447079654b2768d7e674
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-02-09 12:57:30 +0100

    config: custom-privapp-permissions: Add permissions for Updater
    Change-Id: If706b907fcad2a04f4021de5d65d8a3a6c257241

--- config/common.mk
+++ config/common.mk
@@ -318,5 +318,8 @@ $(call inherit-product, vendor/pixel/themepicker/products/themepicker.mk)
 include vendor/lineage/config/version.mk
 include vendor/lineage-priv/keys/keys.mk
 
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/privapp-permissions-custom.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-custom.xml
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
