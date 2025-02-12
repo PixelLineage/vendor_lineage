d54d5966d8caa11f61418134a737334f8a5b0636
Quick ⚡ <quick@disroot.org>
2025-02-12 08:12:25 +0530

    Revert "overlay: Import themed icons from arrow-13.1"
    This reverts commit 32b19a94492fd9174ffb8de46d225363a3c2f576.

--- config/common.mk
+++ config/common.mk
@@ -265,8 +265,7 @@ PRODUCT_PACKAGE_OVERLAYS += \
 
 PRODUCT_PACKAGES += \
     DocumentsUIOverlay \
-    NetworkStackOverlay \
-    ThemedIconsOverlay
+    NetworkStackOverlay
 
 # Translations
 CUSTOM_LOCALES += \
