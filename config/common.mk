fiqri19102002/vendor_aosp

config: custom-privapp-permissions: Add permissions for Updater
Change-Id: If706b907fcad2a04f4021de5d65d8a3a6c257241

config/common.mk
@@ -285,4 +285,7 @@ include vendor/aosp/config/version.mk
 # OTA
 $(call inherit-product, vendor/aosp/config/ota.mk)
 
+PRODUCT_COPY_FILES += \
+    vendor/aosp/config/permissions/privapp-permissions-custom.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-custom.xml
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk

PixelLineage/vendor_lineage

config/common.mk
@@ -302,5 +302,8 @@ $(call inherit-product, vendor/gms/products/gms.mk)
 include vendor/lineage/config/version.mk
 include vendor/lineage-priv/keys/keys.mk
 
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/privapp-permissions-custom.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-custom.xml
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk