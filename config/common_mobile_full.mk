735bc5040bc2a9abaa5f0f2171816554a13fefb8
Quick ⚡ <quick@disroot.org>
2026-03-24 21:32:33 +0530

    vendor: Wipe default fonts
    Change-Id: Ia8428cfc833f972b513c1bb25c7b6cd7987ac93b

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -3,9 +3,6 @@ $(call inherit-product, vendor/lineage/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
-# Include GoogleSansFlex font
-$(call inherit-product-if-exists, external/google-fonts/google-sans-flex/fonts.mk)
-
 # Apps
 ifneq ($(PRODUCT_NO_CAMERA),true)
 PRODUCT_PACKAGES += \
@@ -16,11 +13,6 @@ endif
 PRODUCT_PACKAGES += \
     unrar \
     zstd
-
-# Fonts
-PRODUCT_PACKAGES += \
-    fonts_customization.xml \
-    FontGoogleSansFlexOverlay
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
